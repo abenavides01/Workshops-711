48c234e01a7135ca36598096de81be08790f8e27
abenavides01 <andrepaobena@gmail.com>
2025-02-02 17:19:56 -0600

    workshop2 listo

---
+++ Workshop2/server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require("cors");
+const app = express();
+
+
+// check for cors
+app.use(cors({
+  domains: '*',
+  methods: "*"
+}));
+
+app.get('/tipocambio', function (req, res) {
+  res.send(`{
+    "TipoCompraDolares" : "608",
+    "TipoVentaDolares" : "621",
+    "TipoCompraEuros" : "731.85",
+    "TipoVentaEuros" : "761.9"
+  }`);
+});
+
+
+app.listen(3000, () => console.log(`Example app listening on port 3000!`))
